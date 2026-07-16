a8575eea300f69e4e3ac53fa9b83a32661a97aec
GitControl <theendofalyssa@gmail.com>
2026-07-16 17:06:42 +0500

    style: final redesign of balance block with candy emojis and new phrasing

--- api/handlers/balance.py
+++ api/handlers/balance.py
@@ -15,8 +15,8 @@ async def balance_handler(message: Message):
         user = await get_or_create_user(session, message.from_user.id)
         await message.answer(
             f"🍓 <b>ТВОЙ КОШЕЛЕК</b>\n"
-            f"─── ʚ 🍓 ɞ ───\n\n"
-            f"Внутри приятно звенят Лапкоины. Ты можешь потратить их в «Магазине чудес».\n\n"
-            f"🍓 <b>ТЕКУЩИЙ БАЛАНС:</b>\n"
-            f"➔ <code>{user.balance}</code> Лапкоинов"
+            f"─── ʚ 🍬 ɞ ───\n\n"
+            f"Внутри приятно звенят Лапкоины. Ты можешь потратить их в Магазине чудес.\n\n"
+            f"🍓 <b>ТЕКУЩИЙ БАЛАНС</b>\n"
+            f"У тебя есть ➔ <code>{user.balance}</code> Лапкоинов"
         )
